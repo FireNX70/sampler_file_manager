2787e3d80f2fdbab8bc947379ea4b5db552f14a6
FireNX70 <firenx70@gmail.com>
2026-01-04 13:10:25 +0100

    Default to the Fusion style on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 ﻿#include <QApplication>
 
+#if _WIN32
+	#include <QStyleFactory>
+#endif
+
 #include "GUI/main_window.hpp"
 
 constexpr char NAME[] = "Sampler file manager";
 
 int main(int argc, char **argv)
 {
+	#if _WIN32
+		/*On Windows, the default "windowsvista" style doesn't support dark mode
+		(even though the old "windows" style does), so default to Fusion.*/
+		QApplication::setStyle(QStyleFactory::create("Fusion"));
+	#endif
+
 	QCoreApplication::setOrganizationName(QString(NAME));
 	QCoreApplication::setApplicationName(QString(NAME));
 
